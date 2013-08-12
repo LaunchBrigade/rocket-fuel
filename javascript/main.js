c10ddf6cfd024ee7bdf3810cb1a82559d29de482
Jacob Williams <SuperGeekJake@users.noreply.github.com>
2013-08-12 09:15:36 -0700

    Removed unnecessary console.log.

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,6 @@
 jQuery('img').error(function() {
 	var src = jQuery(this).attr('src');
 	if ( src.search('.svg') != -1 ) {
-		console.log('It worked!');
 		src = src.replace('.svg', '.png');
 		jQuery(this).attr('src', src);
 	}
