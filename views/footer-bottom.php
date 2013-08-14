e409e2c2d14b011338c45c9867cdd138f123308a
Jacob Williams <supergeekjake@gmail.com>
2013-08-13 22:20:32 -0700

    Screenshot, copyright improvement, theme tags.

--- views/footer-bottom.php
+++ views/footer-bottom.php
@@ -1,8 +1,9 @@
 <div class="footer-bottom">
 	<div class="copyright">
-		<p><small><?php // copyright ?>&copy; 2013 <?php bloginfo( 'name' ); ?>. All Rights Reserved.</small></p>
+		<p><small>&copy; 2013 <?php bloginfo( 'name' ); ?>. All Rights Reserved.</small></p>
 	</div>
-    <div class="top-shortcut">
-        <p><small><a href="#top">Top <?php // upwards arrow ?>&uarr;</a></small></p>
-    </div>
+		<div class="top-shortcut">
+				<?php $theme = wp_get_theme(); ?>
+				<p><small>Crafted by <?php echo $theme->Author; ?> <a href="#top">Top &uarr;</a></small></p>
+		</div>
 </div>
