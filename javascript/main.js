95b93f103b0d6b5119559ff475a9930fbf87e697
Jacob Williams <SuperGeekJake@users.noreply.github.com>
2013-11-11 11:22:26 -0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,8 @@
 /**
+ * Warning: Must be included at end of HTML document.
+ */
+
+/**
  * On SVG error, load PNG fallback
  * @return {null}
  */
@@ -8,4 +12,13 @@ jQuery('img').error(function() {
 		src = src.replace('.svg', '.png');
 		jQuery(this).attr('src', src);
 	}
+});
+
+/**
+ * Smoothly animated back-to-top
+ * @return {null}
+ */
+jQuery("a[href='#top']").click( function( event ) {
+	event.preventDefault();
+	jQuery("html, body").animate({ scrollTop: 0 }, 400);
 });
