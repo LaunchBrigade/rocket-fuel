eb4bb21d4e369c1c105adb6c3ed6d36815ae9651
Jacob Williams <SuperGeekJake@users.noreply.github.com>
2013-12-27 12:17:36 -0800

    Fixed printf.

--- views/loop.php
+++ views/loop.php
@@ -16,14 +16,14 @@ if (have_posts()) : while (have_posts()) : the_post(); //=======================
 		</div>
 		<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 		<p class="byline vcard"><?php
-			printf( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author(), get_the_category_list(', '));
+			printf( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author(), get_the_category_list(', '));
 		?></p>
 	</header> <!-- end article header -->
 
 	<section class="entry-content">
 		<?php the_excerpt(); ?>
 	</section> <!-- end article section -->
-
+	
 	<footer class="article-footer">
 		<p class="tags"><?php the_tags('<span class="tags-title">' . 'Tags:' . '</span> ', ', ', ''); ?></p>
 	</footer> <!-- end article footer -->
