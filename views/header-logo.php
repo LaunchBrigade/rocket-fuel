7d254d1d6958cf70ac1389896be9fbaf25d1d988
Albert <g12een7@gmail.com>
2014-02-13 15:07:47 -0800

    Update header-logo.php

--- views/header-logo.php
+++ views/header-logo.php
@@ -1,10 +1,5 @@
 <div class="branding">
 	<a href="<?php echo home_url(); ?>">
 		<?php get_bloginfo( 'name' ); ?>
-		<!-- <img
-			id="logo" class="svg"
-			src="<?php echo get_stylesheet_directory_uri() ?>/images/logo.svg"
-			alt="<?php bloginfo( 'name' ); ?>"
-		> -->
 	</a>
 </div>
