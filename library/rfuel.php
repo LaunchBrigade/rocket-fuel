b2bdc2388db3927c7e7b426856d91726d458e452
Jacob Williams <jake@jacobwilliams.me>
2014-03-19 11:51:49 -0700

    Added documentation

--- library/rfuel.php
+++ library/rfuel.php
@@ -1,6 +1,10 @@
 <?php
 
-
+/**
+ * Get content template for archive based on post-type.
+ * @global $post
+ * @return void
+ */
 function get_archive_content() {
 	$post_type = get_post_type();
 	get_template_part( 'content', $post_type );
