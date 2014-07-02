5de5a7c112bebc57feaec8501f491377d868a75b
Jacob Williams <supergeekjake@gmail.com>
2014-07-02 15:12:01 -0700

    Fixed pagination.

--- views/loop-pagination.php
+++ views/loop-pagination.php
@@ -1,17 +1,33 @@
-<?php
-global $wp_query;
-
-if ($wp_query->max_num_pages > 1) : ?>
+<?php if ($GLOBALS['wp_query']->max_num_pages > 1) : ?>
 <!-- Pagination -->
 <div class="pagination">
 	<?php
-		$current_page = max(1, get_query_var('paged'));
-		echo paginate_links(array(  
-			'base' => get_pagenum_link(1) . '%_%',  
-			'format' => '/page/%#%',  
-			'current' => $current_page,  
-			'total' => $wp_query->max_num_pages,  
-		)); 
+		$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+		$pagenum_link = html_entity_decode( get_pagenum_link() );
+		$query_args   = array();
+		$url_parts    = explode( '?', $pagenum_link );
+
+		if ( isset( $url_parts[1] ) ) {
+			wp_parse_str( $url_parts[1], $query_args );
+		}
+
+		$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+		$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+		$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+		$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+		// Display paginated links.
+		echo paginate_links( array(
+			'base'     => $pagenum_link,
+			'format'   => $format,
+			'total'    => $GLOBALS['wp_query']->max_num_pages,
+			'current'  => $paged,
+			'mid_size' => 1,
+			'add_args' => array_map( 'urlencode', $query_args ),
+			'prev_text' => '&larr; Previous',
+			'next_text' => 'Next &rarr;',
+		));
 	?>
 </div>
 <?php endif; ?>
