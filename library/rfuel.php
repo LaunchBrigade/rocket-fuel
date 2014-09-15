01df341e1fa4c71a955189da91170b890457cd38
Jacob Williams <jwilliams@launchbrigade.com>
2014-09-15 12:26:13 -0700

    Corrected path to TGM class

--- library/rfuel.php
+++ library/rfuel.php
@@ -4,8 +4,9 @@ $dir_path = trailingslashit( get_template_directory() ) . 'library/';
 
 // Sidebar Registration
 require_once( $dir_path . 'sidebars.php' );
+
 // Plugin Activation
-require_once( $dir_path . 'plugin-activation/class-tgm-plugin-activation.php' );
+require_once( $dir_path . 'plugin-activation/tgm-plugin-activation/auto-install.php' );
 
 /**
  * Get content template for archive based on post-type.
