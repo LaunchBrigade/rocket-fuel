7a5152df2c6a86e83afac09cc09e3088663cd860
Jacob Williams <jwilliams@launchbrigade.com>
2014-09-18 13:13:08 -0700

    Removed TGMPA require

--- library/rfuel.php
+++ library/rfuel.php
@@ -5,9 +5,6 @@ $dir_path = trailingslashit( get_template_directory() ) . 'library/';
 // Sidebar Registration
 require_once( $dir_path . 'sidebars.php' );
 
-// Plugin Activation
-require_once( $dir_path . 'plugin-activation/tgm-plugin-activation/auto-install.php' );
-
 /**
  * Get content template for archive based on post-type.
  * @global $post
